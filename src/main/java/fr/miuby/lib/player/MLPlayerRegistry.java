e6f79f36e3bc2f1728ec90501e499e4c6e47ef12
charry-gabriel <gabriel.charry@gmail.com>
2026-02-15 21:25:17 +0100

    Add multiKeyRegistry for MLPlayerRegistry

--- src/main/java/fr/miuby/lib/player/MLPlayerRegistry.java
+++ src/main/java/fr/miuby/lib/player/MLPlayerRegistry.java
@@ -1,19 +1,30 @@
 package fr.miuby.lib.player;
 
+import fr.miuby.lib.utils.MultiKeyRegistry;
+import org.bukkit.entity.Player;
+
 import java.util.*;
 
 public class MLPlayerRegistry<T extends MLPlayer> {
-    private final Map<UUID, T> players = new HashMap<>();
+    private final MultiKeyRegistry<T> players = new MultiKeyRegistry<>();
 
     public void register(T player) {
-        players.put(player.getUuid(), player);
+        players.register(player, player.getUuid(), player.getPseudo(), player.getPlayer());
     }
 
     public T get(UUID uuid) {
         return players.get(uuid);
     }
 
+    public T get(String pseudo) {
+        return players.get(pseudo);
+    }
+
+    public T get(Player player) {
+        return players.get(player);
+    }
+
     public Collection<T> getAll() {
-        return new ArrayList<>(players.values());
+        return players.getAll();
     }
 }
